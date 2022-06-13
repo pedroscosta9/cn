9963f98bd8acf005ae4fc0dd65dd8b147cb76ae1
SimaoFernandes <simaopsfernandes100@gmail.com>
2022-06-13 20:40:06 +0100

    a

--- main/client/src/component/Home/Home.js
+++ main/client/src/component/Home/Home.js
@@ -5,14 +5,19 @@ import "./home.css"
 
 
 function Home() {
-
-
     return (
         <div className="container-home">
-            <div className="rooms" >
-                <Rooms>
+            <div className="rooms-block">
+                <div className="rooms-header">
+                    <div className="rooms-card">
+                        <h1 className="title-rooms">Rooms</h1>
+                    </div>
+                </div>
+                <div className="rooms" >
+                    <Rooms>
 
-                </Rooms>
+                    </Rooms>
+                </div>
             </div>
             <div className="friends">
                 <Friends>
